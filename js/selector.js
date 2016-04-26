dbbe18717d6d94dc08d68cef92da6745ee3954af
Antoine Amara <amara.antoine@gmail.com>
2016-04-26 13:57:34 +0200

    mise en place du js pour le selector

---
+++ js/selector.js
@@ -0,0 +1,49 @@
+$(window).load(function(){
+
+  $(document).ready(function (){
+
+    $('div').mouseup(function (e){
+      var selected = window.getSelection();
+      console.info(selected.toString())
+      if(selected.toString() != '') {
+        btn = $('<button\>', {
+          text: 'Important',
+          class: 'importantbtn',
+          click: function() {
+            $('.selected').attr('id', 'rightclick')
+            $('.selected').attr('class', 'important_1')
+            $('.importantbtn').remove()
+
+            $('#rightclick').on('contextmenu', function(e) {
+              console.info('right click')
+              elmt = $(this)
+              cl = $(this).attr('class')
+              console.info(cl)
+              btn = $('<button\>', {
+                text: '+',
+                class: 'important+',
+                click: function() {
+                  classsplit = elmt.attr('class').split('_')
+                  console.info(classsplit);
+                  num = parseInt(classsplit[1])
+                  num += 1
+                  newclass = 'important_'+num
+                  console.info(newclass)
+                  elmt.attr('class', newclass)
+                }
+              })
+              $('.'+cl).append(btn).end()
+            })
+          }
+        })
+        spn = '<span class="selected">'+selected+'</span>'
+
+        $('.context').html($('.context').html().replace(selected.toString(), spn))
+        $('.selected').append(btn).end()
+      }
+    })
+    $('div').mousedown(function (e){
+    })
+  });
+
+});
