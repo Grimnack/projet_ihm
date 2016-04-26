1ba6e76a2b97a5f7b88e7418beaf9ce375354394
qvandekadsye <qvandekadsye@gmail.com>
2016-04-26 09:55:04 +0200

    mise en page

--- js/import.js
+++ js/import.js
@@ -9,7 +9,7 @@ function impor()
     div.attr('style',"width:100px; margin:0 auto;");
     div.attr('onclick',"window.open('menu.html')");
     var image = $('<\img>');
-    image.attr('src','text.png');
+    image.attr('src','../text.png');
     image.attr('style',"width:128px;height:128px;");
     div.append(image);
     $('.parentid').append(div);
