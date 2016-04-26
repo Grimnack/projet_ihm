61f5994f415f623fc68c11d4284f7db556102334
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-04-26 11:48:11 +0200

    Updated idk

--- js/adviser.js
+++ js/adviser.js
@@ -1,11 +1,34 @@
 var adviser = $('#adviser');
 
-adviser.popover({
-  container: "body",
-  content: "Bonjour ! Je suis votre adviser, <strong>Mimie Mathy</strong> !",
-  html: true
-});
+function pop(text) {
+  adviser.popover('destroy');
+  setTimeout(function() {
+    adviser.popover({
+      container: "body",
+      content: text,
+      html: true,
+    });
+    adviser.popover('show');
+}, 200);
+}
 
 $(document).ready(function() {
-  adviser.popover('show');
+  pop("Bonjour ! Je suis votre adviser, <strong>Mimie Mathy</strong> ! \
+  Je suis ici pour vous assister dans votre longue quête pour la réussite !");
+
+  $(document).keypress(function() {
+    pop("Nous allons commencer ensemble. Cliquez sur les boutons “Éditeur” \
+    ou “Charger fichier” pour commencer en un claquement de doigt !");
+
+    $("#menu-item-editor").popover({
+      container: "body",
+      content: "Ici !",
+      html: true,
+      placement: 'bottom',
+    });
+
+    setTimeout(function() {
+      $("#menu-item-editor").popover('show');
+    }, 1000);
+  });
 });
